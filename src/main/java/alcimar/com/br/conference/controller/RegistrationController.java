675bda82e9839ab59e3a46855869b491fd9396bb
Joao Alcimar <joao.alcimar.junior@gmail.com>
2021-07-28 15:57:00 -0300

    Bind registration model to its controller

--- src/main/java/alcimar/com/br/conference/controller/RegistrationController.java
+++ src/main/java/alcimar/com/br/conference/controller/RegistrationController.java
@@ -1,15 +1,17 @@
 package alcimar.com.br.conference.controller;
 
+import alcimar.com.br.conference.model.RegistrationModel;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
     @GetMapping("registration")
-    public String getRegistration(Map<String, Object> model){
+    public String getRegistration(@ModelAttribute ("registration") RegistrationModel registration){
         return "registration";
     }
 }
---
+++ src/main/java/alcimar/com/br/conference/model/RegistrationModel.java
@@ -0,0 +1,14 @@
+package alcimar.com.br.conference.model;
+
+public class RegistrationModel {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
