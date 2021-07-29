98600526d3d7a27bf764d73e9c7f330dbcceceed
Joao Alcimar <joao.alcimar.junior@gmail.com>
2021-07-29 10:11:09 -0300

    Add some persistency to project

--- src/main/java/alcimar/com/br/conference/controller/UserController.java
+++ src/main/java/alcimar/com/br/conference/controller/UserController.java
@@ -1,8 +1,6 @@
 package alcimar.com.br.conference.controller;
 
-import alcimar.com.br.conference.model.RegistrationModel;
 import alcimar.com.br.conference.model.UserModel;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,6 +16,13 @@ public class UserController {
         user.setFirstName(firstName);
         user.setLastName(lastName);
         user.setAge(age);
+        return user;
+    }
+
+    @PostMapping("/user")
+    public UserModel postUser(UserModel user){
+        System.out.println("User firstname:" + user.getFirstName());
+
         return user;
     }
 
